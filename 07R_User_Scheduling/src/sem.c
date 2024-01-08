08e24a173bb35a807f8a546db73c33383080c26a
Zichen Yang <z.yang@tu-bs.de>
2024-01-08 17:26:58 +0100

    cnm

---
+++ 07R_User_Scheduling/src/sem.c
@@ -0,0 +1,35 @@
+#include "sem.h"
+
+#include "queue.h"
+#include "tcb.h"
+#include "threads.h"
+
+Semaphore_t* semaphore_create(int initial_value) {
+    // WRITE YOUR CODE HERE
+
+    // delete the following lines if you are implementing this function
+    (void)initial_value; //mark initial_value used for the compiler
+
+    return NULL;
+}
+
+void semaphore_destroy(Semaphore_t* sem) {
+    // WRITE YOUR CODE HERE
+
+    // delete the following lines if you are implementing this function
+    (void)sem; //mark sem used for the compiler
+}
+
+void sem_wait(Semaphore_t* sem) {
+    // WRITE YOUR CODE HERE
+
+    // delete the following lines if you are implementing this function
+    (void)sem; //mark sem used for the compiler
+}
+
+void sem_post(Semaphore_t* sem) {
+    // WRITE YOUR CODE HERE
+
+    // delete the following lines if you are implementing this function
+    (void)sem; //mark sem used for the compiler
+}
